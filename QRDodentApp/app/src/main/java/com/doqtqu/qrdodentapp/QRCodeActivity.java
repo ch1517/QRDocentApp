8095b64075dfc30047130473919dcfbf2d1a3fce
SecondWind <25702775+ch1517@users.noreply.github.com>
2019-09-18 23:01:41 +0900

    QrPage add

--- QRDodentApp/app/src/main/java/com/doqtqu/qrdodentapp/QRCodeActivity.java
+++ QRDodentApp/app/src/main/java/com/doqtqu/qrdodentapp/QRCodeActivity.java
@@ -2,7 +2,12 @@ package com.doqtqu.qrdodentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class QRCodeActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class QRCodeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
+
+        new IntentIntegrator(this).initiateScan();
+
     }
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        //  com.google.zxing.integration.android.IntentIntegrator.REQUEST_CODE
+        //  = 0x0000c0de; // Only use bottom 16 bits
+        if (requestCode == IntentIntegrator.REQUEST_CODE) {
+            IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+            if (result == null) {
+                // 취소됨
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                // 스캔된 QRCode --> result.getContents()
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
 }
